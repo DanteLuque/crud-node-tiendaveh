4501784b420ae7225b9e6722239b9f9669e79edf
DanDev1004 <dandeveloper1004@gmail.com>
2025-08-29 08:58:50 -0500

    refactor: 💡 module shared y campos de auditoria

--- src/config/mysql/Database/base.sql
+++ src/config/mysql/Database/base.sql
@@ -7,11 +7,14 @@ CREATE TABLE vehiculos(
     modelo 	VARCHAR(30) NOT NULL,
     color  	VARCHAR(20) NOT NULL,
     precio	DECIMAL(9,2) NOT NULL,
-    placa	CHAR(7) UNIQUE NOT NULL
+    placa	CHAR(7) NOT NULL,     -- UNICO
+    created_at DATETIME NULL,
+    updated_at DATETIME NULL,
+    deleted_at DATETIME NULL
 )ENGINE INNODB;
 
 INSERT INTO vehiculos VALUES
-	(NULL, 'Nissan', 'Frontier', 'gris', 145000, 'ABC-111'),
-    (NULL, 'Toyota', 'Hilux', 'blanco', 98000, 'ABC-222');
+	(NULL, 'Nissan', 'Frontier', 'gris', 145000, 'ABC-111', NOW(), NOW(), NULL),
+    (NULL, 'Toyota', 'Hilux', 'blanco', 98000, 'ABC-222', NOW(), NOW(), NULL);
     
 SELECT * FROM vehiculos;
